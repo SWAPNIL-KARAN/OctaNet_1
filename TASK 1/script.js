091ac62d4ace41fc888c561732b296c7bdd01f90
SWAPNIL KARAN <166383097+SWAPNIL-KARAN@users.noreply.github.com>
2024-07-17 21:58:37 +0530

    Add files via upload

---
+++ TASK 1/script.js
@@ -0,0 +1,8 @@
+function register() {
+    alert("Registration coming soon!");
+}
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for your message! We will get back to you soon.");
+});
